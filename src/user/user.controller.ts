cdb9c3bcba60cf64b35026a972c9a9c4ca6d6ede
S7b0t4 <toni.paun.00@mail.ru>
2024-09-07 18:16:14 +0500

    add functional for user administration

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { createUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
@@ -14,5 +14,17 @@ export class UserController {
   @Post("/create")
   create(@Body() userDto: createUserDto) {
     return this.UserService.createUser(userDto)
+  }
+
+  @ApiOperation({summary: "Получение всех пользователей"})
+  @Get("/get")
+  get(){
+    return this.UserService.getUser()
+  }
+
+  @ApiOperation({summary: "Удаление всех пользователей"})
+  @Post("/delete")
+  delete(){
+    return this.UserService.deleteUsers()
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,10 +3,16 @@ import { createUserDto } from './dto/create-user.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './user.model';
 import { findUserDto } from './dto/find-user.dto';
+import { Sequelize } from 'sequelize-typescript';
+import { Task } from 'src/task/task.model';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User) private userRepository: typeof User) {}
+  constructor(
+    private readonly sequelize: Sequelize,
+    @InjectModel(User) private userRepository: typeof User,
+    @InjectModel(User) private taskRepository: typeof Task,
+    ) {}
 
   async createUser(dto: createUserDto) {
     const userData = await this.userRepository.create(dto);
@@ -21,5 +27,18 @@ export class UserService {
   async getUser() {
     const userBunch = await this.userRepository.findAll()
     return userBunch
+  }
+
+  async deleteUsers() {
+    try{
+      await this.sequelize.query('TRUNCATE TABLE "tasks" RESTART IDENTITY CASCADE;');
+      await this.sequelize.query('TRUNCATE TABLE "users" RESTART IDENTITY CASCADE;');
+
+      return "DELETE"
+    }catch(e){
+      console.log(e)
+      throw new HttpException("Проблема удаления", HttpStatus.BAD_REQUEST)
+    }
+
   }
 }
